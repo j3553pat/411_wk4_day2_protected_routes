8c08a91fabead627f440eb2166d15758cfcb65ba
j3553pat <jessecodedev@gmail.com>
2023-06-07 19:23:04 -0500

    deploy

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TextField, Button, Container } from "@mui/material";
+import cookie from 'cookie'
 
 const Login = () => {
   const navigate = useNavigate();
@@ -23,8 +24,8 @@ const Login = () => {
   const login = (e) => {
     e.preventDefault();
     // set cookie here
-    // set loggedIn = true and max-age = 60*1000 (one minute)
-
+    document.cookie = cookie.serialize("loggedIn=true, maxAge= 1000 * 60")
+    document.cookie = cookie.serialize("loggedIn=null, maxAge = 0");
     navigate("/");
   };
 
