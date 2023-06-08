1d39d88bc7186c06e77a38604e1cad626d5a743e
j3553pat <jessecodedev@gmail.com>
2023-06-07 19:23:49 -0500

    Updates

---
+++ precache-manifest.05bf539b029f5d66aaf6aefde1a7bc32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1835501d2ec9e76e98b568c240b85427",
+    "url": "/411_wk4_day2_protected_routes/index.html"
+  },
+  {
+    "revision": "59c991ed30f0c74f7c3f",
+    "url": "/411_wk4_day2_protected_routes/static/css/main.909db9c4.chunk.css"
+  },
+  {
+    "revision": "a5053180f95a98d22876",
+    "url": "/411_wk4_day2_protected_routes/static/js/2.cf67a152.chunk.js"
+  },
+  {
+    "revision": "59c991ed30f0c74f7c3f",
+    "url": "/411_wk4_day2_protected_routes/static/js/main.90fc35a5.chunk.js"
+  },
+  {
+    "revision": "e1c9e8bec29a5f33d35d",
+    "url": "/411_wk4_day2_protected_routes/static/js/runtime~main.8cfe26ec.js"
+  }
+]);
